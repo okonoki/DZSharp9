a0141c7c713d937ffc6c8ac60bb5bcf103128ffe
okonoki <okonokiss@gmail.com>
2023-08-03 22:59:22 +0500

    task02 is done

--- task02/Program.cs
+++ task02/Program.cs
@@ -2,3 +2,11 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NumbersRec(int n)
+{
+    if (n == 0) return "";
+    else return $"{n}, " + NumbersRec(n - 1);
+}
+
+Console.WriteLine(NumbersRec(5));
